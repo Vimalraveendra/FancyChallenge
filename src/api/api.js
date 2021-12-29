a9b351f2144d699225a6f0cd432c15e7801f3a5d
Vimal Raveendran <vimalraveendra@gmail.com>
2021-12-29 23:15:12 +0100

    updated api component

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,8 @@
 export const fetchData = async (url) => {
-    console.log('uel',url)
+  
     try {
       const response= await fetch(url)
       const data = await response.json();
-      console.log('data',data)
       return data;
     } catch (error) {
       console.log("error", error);
